c0336f8b6b9960b23535868f87523ddee929a523
GOPA SAI VEMANA RAO <gopasaiv@gmail.com>
2025-12-28 12:42:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ app.use(express.json());
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "12345",
-  database: "student_results"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
+
 
 // API to get result by hall ticket number
 app.get("/result/:hallticket", (req, res) => {
